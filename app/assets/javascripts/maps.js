34ca1eca3ad2f37e639ea8cc6e222667858deef7
Leon Entrup <luenneberger@gmail.com>
2018-07-08 17:15:55 +0200

    fix of google maps functionality and safari color fix

---
+++ app/assets/javascripts/maps.js
@@ -0,0 +1,77 @@
+var custom_style = [
+    {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "hue": "#00d5ff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.landcover",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ff0000"
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#ff0000"
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+];
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('hochzeit_map'), {
+    center: {lat: 52.680488, lng: 9.833958},
+    zoom: 8,
+    styles: custom_style,
+  });
+  var marker = new google.maps.Marker({
+    position:{lat:52.67518,lng:9.84277},
+    map:map,
+  });
+}
+
